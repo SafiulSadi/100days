f10783489b0f4f16bcbbcb988fbd66823a4b5b50
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-02-19 15:25:53 +0600

    work on arrays

--- dojo/apnaCollege/Array.java
+++ dojo/apnaCollege/Array.java
@@ -58,15 +58,70 @@ public class Array {
             end--;
         }
     }
+    public static void pair_print(int arr[]){
+        int cnt = 0;
+        for(int i= 0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("(" + arr[i]+", " +arr[j] + ")" );
+                cnt ++;
+            }
+        }
+        System.out.println(cnt);
+    }
+    public static void printSubarrays(int arr[]){
+        int start = 0;
+        int end = start + 1;
+        for(int i = 0; i < arr.length; i++){
+            System.out.print("["+arr[i]+"]");
+
+            for(int j= i +1;j<arr.length; j++ ){
+                System.out.print("[");
+                for(int k=i; k<=j; k++){
+                    System.out.print(+arr[k]+", ");
+                }
+                System.out.print("]");
+                System.out.println();
+            }
+        }
+    }
+    public static void sum_of_subarray(int arr[]){
+        int sum[] = new int[15];
+        int index = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j=i; j<arr.length; j++){
+                int s = 0;
+                for(int k=i; k<=j; k++){
+                    s += arr[k];
+                    
+                }
+                sum[index]= s;
+                index ++;
+            }
+        }
+        Integer max = Integer.MIN_VALUE;
+        Integer min = Integer.MAX_VALUE;
+        for(int i = 0; i< sum.length; i++){
+            if(max < sum[i]){
+                max = sum[i];
+            }
+            if(min > sum[i]){
+                min = sum[i];
+            }
+
+        }
+        System.out.println("max is: " +max);
+        System.out.println("min is: " +min);
+
+    }
     public static void main(String[] args) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int numbers[] = {2, 4, 6, 8, 10};
+        int numberss[] = {1, -2, 6, -1, 3};
         String menu[] = {"coke", "fanta", "dew"};
         int key = 12;
-        reverse_array(numbers);
-        for (int i : numbers) {
-            
-            System.out.print(i+", ");
-        }
+        
+        sum_of_subarray(numberss);
+        
     }
     
 }
