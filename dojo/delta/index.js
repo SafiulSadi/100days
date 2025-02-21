b0e98a84c34029d420656cc1a80f65513c955c69
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-02-22 05:53:56 +0600

    js start

---
+++ dojo/delta/index.js
@@ -0,0 +1,14 @@
+let num = "sadi";
+let name = "tony stark";
+console.log(num.length);
+console.log(num[num.length - 1]);
+console.log("apnacollege"+123);
+console.log("".length);               
+console.log(" ".length);               
+// name.split("").forEach(e => {
+//     console.log(e,end=" ");
+// });
+// console.log(name[3]);
+// for(let i=0; i<name.length; i++){
+//     console.log(name[i]);
+// }
