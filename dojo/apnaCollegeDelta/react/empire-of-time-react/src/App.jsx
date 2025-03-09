e12df68c1f324896784727ce16ba02a1ed121aa6
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-03-10 05:59:32 +0600

    start js

--- dojo/apnaCollegeDelta/react/empire-of-time-react/src/App.jsx
+++ dojo/apnaCollegeDelta/react/empire-of-time-react/src/App.jsx
@@ -8,7 +8,7 @@ import Card from './Card'
 import Converse from './Converse';
 import Container from './Container';
 function App() {
-  let hoodies = [{htitle:"Zoro Wanted Poster Hoodie/Tee for winter", price:20, image:zoro}, {htitle:"All Eyes On Me Tee/Hoodie/Hello hoodies for winter and summer", price:20, image:alleye}, {htitle:"Converse Chuck Taylor All Star Low Top", price:65, image:converse}]
+  let hoodies = [{htitle:"Zoro Wanted Poster Tee for winter", price:20, image:zoro}, {htitle:"All Eyes On Me Tee/Hoodie/Hello hoodies for winter and summer", price:20, image:alleye}, {htitle:"Converse Chuck Taylor All Star Low Top", price:65, image:converse}]
   let arr = ["Zoro Wanted Poster Hoodie/Tee for winter", "All Eyes On Me Tee/Hoodie for winter and summer", "Converse Chuck Taylor All Star Low Top"]
   return (
     <>
@@ -18,5 +18,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
