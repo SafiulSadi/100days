cbfbec018be14c1e3829c77a10904e857f7b94cc
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-03-21 06:00:09 +0600

    python

--- dojo/100daysofpython/main.py
+++ dojo/100daysofpython/main.py
@@ -1,22 +1,2 @@
-import bs4
-n = int(input())
 
-while n >0:
-    num = input()
-    arr = input()
-    srr = [ int(i) for i in arr]
-    x = 0
-    curr = 0
-        
-    for i in srr:
-        int(i)
-        if i == 1 and i != curr:
-            x += 1
-            curr = 1
-        if i == 0 and i != curr:
-            x+= 1
-            curr = 0
-    print(x)
-    
-    
-    n -= 1
+print("hello world")
