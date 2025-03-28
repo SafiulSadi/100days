a720c45a6a611b8740196e8164e1a2f821e955bb
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-03-29 03:44:11 +0600

    finish day 31

--- dojo/100daysofpython/day31/main.py
+++ dojo/100daysofpython/day31/main.py
@@ -3,35 +3,50 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("./data/french_words.csv")
-print(data.head())
-to_learn = data.to_dict(orient="records")
-print(to_learn)
-
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else: 
+    print(data.head())
+    to_learn = data.to_dict(orient="records")
+
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     print(current_card["French"])
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
-    window.after(3000, func=flip_card)
-    
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_image, image=card_front_img)
+    print(current_card["English"])
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_title, text="Arabic")
     canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_title, text="English", fill="white")
+    # canvas.itemconfig(card_word, text="Arabic", fill="white")
     canvas.itemconfig(card_image, image=card_back_img)
     
-
+def is_known():
+    print(len(to_learn))
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    
+    next_card()
+    
 window = Tk()
 window.title('Flashy')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 
 
 canvas = Canvas(width=800, height=526)
@@ -50,7 +65,7 @@ unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card
 unknown_button.grid(row=1, column=0)
 
 check_img = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_img, highlightthickness=0, command=next_card)
+known_button = Button(image=check_img, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
--- dojo/100daysofpython/day31/test.py
+++ dojo/100daysofpython/day31/test.py
@@ -1,6 +1,11 @@
 import pandas
+import numpy
 
-data = pandas.read_csv("./data/words.csv")
-words = data.to_dict(orient="records")
-print(data)
+# data = pandas.read_csv("./data/words.csv")
+# words = data.to_dict(orient="records")
+# print(data[0][0])
+
 # 
+arr = numpy.loadtxt("./data/words.csv",
+                 delimiter=",", dtype=str)
+print(arr)
