7dcab85f0b1669501d6d09f514f5b5065f9382a2
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-03-31 08:37:55 +0600

    go through day 32

--- dojo/100daysofpython/day32/main.py
+++ dojo/100daysofpython/day32/main.py
@@ -1,1 +1,9 @@
+import smtplib
+
+my_email = "safiul.sadi1@gmail.com"
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls( )
+
 print("email sending")
+print("finish day 32 of angela's python")
