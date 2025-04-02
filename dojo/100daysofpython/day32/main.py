0038e4f8d31ea6132398b3b7abc62cf0b08a6d19
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-04-03 03:38:51 +0600

    start datetime

--- dojo/100daysofpython/day32/main.py
+++ dojo/100daysofpython/day32/main.py
@@ -1,18 +1,26 @@
-import smtplib
+# import smtplib
 
-my_email = "safiul.sadi1@gmail.com"
-test_email = "safiul.sadi@gmail.com"
-my_password = "cbmlwdaemjpsfnbm"
-subject = "Hello"
-body = "This is the content of the email."
-message = f"Subject:{subject}\n\n{body}"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls( )
+# my_email = "safiul.sadi1@gmail.com"
+# test_email = "safiul.sadi@gmail.com"
+# my_password = "cbmlwdaemjpsfnbm"
+# subject = "Hello"
+# body = "This is the content of the email."
+# message = f"Subject:{subject}\n\n{body}"
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls( )
 
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs=test_email, msg=message)
+# connection.login(user=my_email, password=my_password)
+# connection.sendmail(from_addr=my_email, to_addrs=test_email, msg=message)
 
 
-print("email sending")
+# print("email sending")
 
-connection.close()
+# connection.close()
+
+import datetime as dt
+
+n = dt.datetime.now()
+
+print(n)
+
+
