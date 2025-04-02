42979ee57c0f07be8c5b1d9b4197e983203709a0
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-04-02 07:08:20 +0600

    half way done day32

--- dojo/100daysofpython/day32/main.py
+++ dojo/100daysofpython/day32/main.py
@@ -1,11 +1,18 @@
 import smtplib
 
 my_email = "safiul.sadi1@gmail.com"
-
+test_email = "safiul.sadi@gmail.com"
+my_password = "cbmlwdaemjpsfnbm"
+subject = "Hello"
+body = "This is the content of the email."
+message = f"Subject:{subject}\n\n{body}"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls( )
 
+connection.login(user=my_email, password=my_password)
+connection.sendmail(from_addr=my_email, to_addrs=test_email, msg=message)
+
+
 print("email sending")
-print("finish day 32 of angela's python")
-s = "i do not know if the git is working or not?"
-print(s)
+
+connection.close()
