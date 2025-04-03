0cbc34fa691e5dcf9005955f94833ce903d5cf6d
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-04-04 05:51:22 +0600

    keep trying day 32

--- dojo/100daysofpython/day32/main.py
+++ dojo/100daysofpython/day32/main.py
@@ -1,26 +1,48 @@
-# import smtplib
+import smtplib
 
-# my_email = "safiul.sadi1@gmail.com"
-# test_email = "safiul.sadi@gmail.com"
-# my_password = "cbmlwdaemjpsfnbm"
-# subject = "Hello"
-# body = "This is the content of the email."
-# message = f"Subject:{subject}\n\n{body}"
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls( )
-
-# connection.login(user=my_email, password=my_password)
-# connection.sendmail(from_addr=my_email, to_addrs=test_email, msg=message)
+my_email = "safiul.sadi1@gmail.com"
+test_email = "safiul.sadi@gmail.com"
+my_password = "cbmlwdaemjpsfnbm"
 
 
-# print("email sending")
 
-# connection.close()
 
 import datetime as dt
+import random
 
-n = dt.datetime.now()
+quote = ""
+with open("quotes.txt", "r") as quotes_file:
+    quotes = quotes_file.readlines()
+    print(random.choice(quotes))
+    quote = random.choice(quotes)
+    
+subject = "Hello"
+body = quote
+message = f"Subject:{subject}\n\n{body}"
 
-print(n)
 
+now = dt.datetime.now()
+if now.day == 3:
+    print(quote)
+    
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        
+        connection.starttls( )
+
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=test_email, msg=message)
+
+
+        print("email sending")
+
+
+
+
+day_of_the_week = now.weekday()
+print(day_of_the_week)
+# print(now)
+
+date_of_birth = dt.datetime(year=1992 , month= 5, day=17, hour=10)
+
+print(now.year-date_of_birth.year)
 
